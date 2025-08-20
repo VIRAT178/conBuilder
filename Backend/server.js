c41e1b88d920deaf5f5a0fe0d1290f9c510f81c2
vishal <vishalsinghvicky95@gmail.com>
2025-08-21 00:08:59 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -14,8 +14,22 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
+import cors from 'cors';
+
+const allowedOrigins = [
+  'https://con-builder.vercel.app'
+];
+
 app.use(cors({
-  origin: true,  
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+    
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
 
