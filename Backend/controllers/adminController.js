7e12a9291e4bc8db035af8daf8438f76e4feb710
vishal <vishalsinghvicky95@gmail.com>
2025-08-20 11:13:25 +0530

    other

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -52,7 +52,11 @@ export const login = async (req, res) => {
     }
 
     const token = genrateToken(adminData._id);
-    res.json({ success: true, message: "Logged In Successfully" });
+    return res.json({
+      success: true,
+      message: "Logged In Successfully",
+      token,
+    });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
