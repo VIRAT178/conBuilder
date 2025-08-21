87f0d2ed11ae4a8ecd55d05a355089c2a6c12078
vishal <vishalsinghvicky95@gmail.com>
2025-08-21 17:55:54 +0530

    autho

--- Backend/server.js
+++ Backend/server.js
@@ -14,26 +14,13 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
-const allowedOrigins = [
-  'https://con-builder.vercel.app',
-  'http://localhost:5173',
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-   
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: true,        
+  credentials: true, 
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
 }));
+
 
 app.options('*', cors());
 
